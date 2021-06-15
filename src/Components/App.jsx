268164e0c9b116335e88bfde69ed93f385134503
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2021-06-15 22:12:33 +0530

    Keeper App - Part 2 Complete

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <Note/>
-            <Footer />
-        </div>
-        
-    );
+  return (<div>
+    <Header/> {notes.map(item => <Note key={item.key} heading={item.title} info={item.content}/>)}
+    <Footer/>
+  </div>);
 }
 
 export default App;
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,7 +1,10 @@
-import React from "react"; 
+import React from "react";
 
-function Note() {
-    return (<div className = "note"><h1>This is the note title</h1><p>This is the note content</p></div>);
+function Note(props) {
+  return (<div className="note">
+    <h1>{props.heading}</h1>
+    <p>{props.info}</p>
+  </div>);
 }
 
 export default Note;
