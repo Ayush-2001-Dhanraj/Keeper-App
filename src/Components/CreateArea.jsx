b9226d06bd8a34603a59ecc0bc4551c7782c627a
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2021-06-27 21:57:48 +0530

    Add some styling

--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  function Expand() {
+    setIsExpanded(true);
+  }
   return (<div>
-    <form onSubmit={props.addNote}>
-      <input onChange={props.handleChange} value={props.note.title} name="title" placeholder="Title"/>
-      <textarea onChange={props.handleChange} value={props.note.content} name="content" placeholder="Take a note..." rows="3"/>
-      <button type="submit">Add</button>
+    <form onSubmit={props.addNote} className="create-note">
+      {isExpanded && <input onChange={props.handleChange} value={props.note.title} name="title" placeholder="Title"/>}
+      <textarea onClick={Expand} onChange={props.handleChange} value={props.note.content} name="content" placeholder="Take a note..." rows={(
+          isExpanded)
+          ? "3"
+          : "1"}/>
+      <Zoom in={isExpanded}>
+        <Fab type="submit"><AddIcon/></Fab>
+      </Zoom>
     </form>
   </div>);
 }
